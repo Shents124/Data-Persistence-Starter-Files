9377d4ab4a94f435621896e4634ea7727b10f549
Shents124 <72143563+Shents124@users.noreply.github.com>
2021-06-15 21:24:20 +0700

    Add class SaveData to store name and high score.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,12 @@
-using System;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using System.IO;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public string namePlayer;
-    public int highScore;
+    public string namePlayerHighScore;
+    public int highScore = 0;
     private void Awake()
     {
         if (Instance != null)
@@ -18,17 +17,34 @@ public class GameManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        LoadDataPlayer();
+    }
+    
+    [System.Serializable]
+    class SaveData
+    { 
+        public string namePlayerHighScore;
+        public int highScore;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void SaveDataPlayer()
     {
-        
+        SaveData data = new SaveData();
+        data.namePlayerHighScore = namePlayerHighScore;
+        data.highScore = highScore;
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json",json);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadDataPlayer()
     {
-        
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            highScore = data.highScore;
+            namePlayerHighScore = data.namePlayerHighScore;
+        }
     }
 }
