4551a64ea7a9b694050841c94413f8aa9222f67d
Shents124 <72143563+Shents124@users.noreply.github.com>
2021-06-15 10:14:58 +0700

    Create MenuManager script

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField playerName;
+
+    [SerializeField] private TextMeshProUGUI highScore;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
